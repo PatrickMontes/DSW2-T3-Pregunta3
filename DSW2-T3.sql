d39783d69c55e665671e5f2a0b922f398acb2ab6
PatrickMontes <alexandermontes30p@hotmail.com>
2024-10-24 21:56:00 -0500

    .

--- DSW2-T3.sql
+++ DSW2-T3.sql
@@ -1,30 +1,34 @@
+#DROP DATABASE T3DSW;
+
 CREATE DATABASE T3DSW;
+USE T3DSW;
+
 
 /*--PRIMER PROCESO--*/
 CREATE TABLE Estudiante (
-    id INT(10) PRIMARY KEY,
-    nombre VARCHAR(100),
-    apellido VARCHAR(100),
-    email VARCHAR(100),
-    telefono VARCHAR(15),
-    ciclo_academico VARCHAR(10)
+                            id INT(10) PRIMARY KEY,
+                            nombre VARCHAR(100),
+                            apellido VARCHAR(100),
+                            email VARCHAR(100),
+                            telefono VARCHAR(15),
+                            ciclo_academico VARCHAR(10)
 );
 
 CREATE TABLE Curso (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    nombre VARCHAR(100),
-    creditos INT,
-    ciclo_academico VARCHAR(10)
+                       id INT PRIMARY KEY AUTO_INCREMENT,
+                       nombre VARCHAR(100),
+                       creditos INT,
+                       ciclo_academico VARCHAR(10)
 );
 
 CREATE TABLE Matricula (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    id_estudiante VARCHAR(10),
-    id_curso INT,
-    fecha_matricula DATE,
-    estado VARCHAR(20) CHECK (estado IN ('Activa', 'Cancelada')),
-    FOREIGN KEY (id_estudiante) REFERENCES Estudiante(id),
-    FOREIGN KEY (id_curso) REFERENCES Curso(id)
+                           id INT PRIMARY KEY AUTO_INCREMENT,
+                           id_estudiante INT,
+                           id_curso INT,
+                           fecha_matricula DATE,
+                           estado VARCHAR(20) CHECK (estado IN ('Activa', 'Cancelada')),
+                           FOREIGN KEY (id_estudiante) REFERENCES Estudiante(id),
+                           FOREIGN KEY (id_curso) REFERENCES Curso(id)
 );
 
 
@@ -32,19 +36,19 @@ CREATE TABLE Matricula (
 #Tabla Estudiante
 
 CREATE TABLE Pago (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    id_estudiante VARCHAR(10),
-    fecha_pago DATE,
-    monto DECIMAL(10, 2),
-    estado VARCHAR(20) CHECK (estado IN ('Pendiente', 'Pagado', 'Vencido')),
-    FOREIGN KEY (id_estudiante) REFERENCES Estudiante(id)
+                      id INT PRIMARY KEY AUTO_INCREMENT,
+                      id_estudiante INT,
+                      fecha_pago DATE,
+                      monto DECIMAL(10, 2),
+                      estado VARCHAR(20) CHECK (estado IN ('Pendiente', 'Pagado', 'Vencido')),
+                      FOREIGN KEY (id_estudiante) REFERENCES Estudiante(id)
 );
 
 CREATE TABLE Detalle_Pago (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    id_pago INT,
-    monto DECIMAL(10, 2),
-    FOREIGN KEY (id_pago) REFERENCES Pago(id)
+                              id INT PRIMARY KEY AUTO_INCREMENT,
+                              id_pago INT,
+                              monto DECIMAL(10, 2),
+                              FOREIGN KEY (id_pago) REFERENCES Pago(id)
 );
 
 
@@ -52,11 +56,11 @@ CREATE TABLE Detalle_Pago (
 #Tabla Estudiante
 #Tabla Curso
 CREATE TABLE Asistencia (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    id_estudiante VARCHAR(10),
-    id_curso INT,
-    fecha DATE,
-    estado VARCHAR(20) CHECK (estado IN ('Presente', 'Ausente', 'Justificado')),
-    FOREIGN KEY (id_estudiante) REFERENCES Estudiante(id),
-    FOREIGN KEY (id_curso) REFERENCES Curso(id)
+                            id INT PRIMARY KEY AUTO_INCREMENT,
+                            id_estudiante INT,
+                            id_curso INT,
+                            fecha DATE,
+                            estado VARCHAR(20) CHECK (estado IN ('Presente', 'Ausente', 'Justificado')),
+                            FOREIGN KEY (id_estudiante) REFERENCES Estudiante(id),
+                            FOREIGN KEY (id_curso) REFERENCES Curso(id)
 );
